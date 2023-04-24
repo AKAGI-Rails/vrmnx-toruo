cd548b34898b16b0d9acc4b0595e4057250acc2f
AKAGI-Rails <akag99@gmail.com>
2023-04-24 23:42:01 +0900

    made setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='Toruo',
+      version='3.2.0',
+      py_modules=['toruo'],
+      author='AKAGI',
+      description='VRMNX Extended Camerawork'
+      )
