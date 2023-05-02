7294ac9a438b47e2332206841a56995d3ee3d49f
AKAGI-Rails <akag99@gmail.com>
2023-05-03 01:31:11 +0900

    update setup.py for v.3.3.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='Toruo',
-      version='3.2.0',
+      version='3.3.0',
       py_modules=['toruo'],
       author='AKAGI',
       description='VRMNX Extended Camerawork'
